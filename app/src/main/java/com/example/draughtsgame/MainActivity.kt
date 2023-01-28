5de4208ed67d479f8745a17170b55bed0e47120a
Patrick941 <farmerp@tcd.ie>
2023-01-28 21:59:10 +0000

    Board design finished with buttons to read touches on individual squares

--- app/src/main/java/com/example/draughtsgame/MainActivity.kt
+++ app/src/main/java/com/example/draughtsgame/MainActivity.kt
@@ -10,16 +10,26 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import android.graphics.drawable.shapes.Shape
+import android.widget.Button
+
+import java.net.IDN
 
 class MainActivity : AppCompatActivity() {
     private var thisName = "MainActivity"
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+            val squares = arrayOf(findViewById<Button>(R.id.a1button), findViewById(R.id.a2button), findViewById(R.id.a3button), findViewById(R.id.a4button), findViewById(R.id.a5button), findViewById(R.id.a6button), findViewById(R.id.a7button), findViewById(R.id.a8button), findViewById(R.id.b1button), findViewById(R.id.b2button), findViewById(R.id.b3button), findViewById(R.id.b4button), findViewById(R.id.b5button), findViewById(R.id.b6button), findViewById(R.id.b7button), findViewById(R.id.b8button), findViewById(R.id.c1button), findViewById(R.id.c2button), findViewById(R.id.c3button), findViewById(R.id.c4button), findViewById(R.id.c5button), findViewById(R.id.c6button), findViewById(R.id.c7button), findViewById(R.id.c8button), findViewById(R.id.d1button), findViewById(R.id.d2button), findViewById(R.id.d3button), findViewById(R.id.d4button), findViewById(R.id.d5button), findViewById(R.id.d6button), findViewById(R.id.d7button), findViewById(R.id.d8button), findViewById(R.id.e1button),
+            findViewById(R.id.e2button), findViewById(R.id.e2button), findViewById(R.id.e3button), findViewById(R.id.e4button), findViewById(R.id.e5button), findViewById(R.id.e6button), findViewById(R.id.e7button), findViewById(R.id.e8button), findViewById(R.id.f1button), findViewById(R.id.f2button), findViewById(R.id.f2button), findViewById(R.id.f3button), findViewById(R.id.f4button), findViewById(R.id.f5button), findViewById(R.id.f6button), findViewById(R.id.f7button), findViewById(R.id.f8button),
+            findViewById(R.id.g1button), findViewById(R.id.g2button), findViewById(R.id.g3button), findViewById(R.id.g4button), findViewById(R.id.g5button), findViewById(R.id.g6button), findViewById(R.id.g7button), findViewById(R.id.g8button), findViewById(R.id.h1button), findViewById(R.id.h2button), findViewById(R.id.h3button), findViewById(R.id.h4button), findViewById(R.id.h5button), findViewById(R.id.h6button), findViewById(R.id.h7button), findViewById(R.id.h8button))
+            squares.forEach{
+
+                it.setBackgroundColor(Color.TRANSPARENT)
+                it.setOnClickListener {
+                    Log.i("myTag", "$it was pressed in $thisName")
+                }
+            }
         Log.i("myTag", "creating $thisName")
 
         val player1CapturedRecycler = findViewById<RecyclerView>(R.id.Player1Captured)
